36ca1765f9ac35fda7a8de35219d2402836b8b90
zlydenko <anton.zlydenko@gmail.com>
2018-05-02 17:18:24 +0300

    added line chart

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import React, { Component, Fragment } from "react";
 import styled, { keyframes } from "styled-components";
-
+import "../node_modules/react-vis/dist/style.css";
 import { Loading, Text, Fall, Rise } from "./styled";
 import formatPrice from "./utils";
+import { XYPlot, makeWidthFlexible, GradientDefs, LineSeries } from "react-vis";
+
+const FlexibleXYPlot = makeWidthFlexible(XYPlot);
 
 class App extends Component {
   state = {
+    history: [],
     data: [],
     intervalId: {}
   };
@@ -44,7 +48,27 @@ class App extends Component {
       .catch(err => console.log(err));
   };
 
+  getHistory = () => {
+    fetch(
+      "https://min-api.cryptocompare.com/data/histominute?fsym=BTC&tsym=USD&limit=60&aggregate=3&e=CCCAGG"
+    )
+      .then(res => res.json())
+      .then(data => {
+        const arr = data.Data.map(value => {
+          return {
+            x: value.time,
+            y: value.close
+          };
+        });
+        console.log(arr);
+        this.setState({
+          history: arr
+        });
+      });
+  };
+
   componentDidMount() {
+    this.getHistory();
     const timer = setInterval(this.getData, 5000);
     this.setState({ intervalId: timer });
   }
@@ -58,14 +82,75 @@ class App extends Component {
     const newPrice = data[data.length - 1];
     const prevPrice = data[data.length - 2];
 
+    const gradient = (
+      <GradientDefs>
+        <linearGradient
+          id="myGradient"
+          gradientUnits="userSpaceOnUse"
+          x1="0"
+          y1="0"
+          x2="500"
+          y2="500"
+        >
+          <stop offset="0%" stopColor="#EDDE5D" />
+          <stop offset="25%" stopColor="gold" />
+          <stop offset="55%" stopColor="#fe8c00" />
+          <stop offset="100%" stopColor="#f83600" />
+        </linearGradient>
+      </GradientDefs>
+    );
+
     if (data.length === 0) {
       return <Loading />;
     } else if (!prevPrice || prevPrice.x === newPrice.x) {
-      return <Text>{formatPrice(newPrice.x)}</Text>;
+      return (
+        <Fragment>
+          <Text>{formatPrice(newPrice.x)}</Text>
+          <div>
+            {this.state.history && (
+              <FlexibleXYPlot height={200}>
+                {gradient}
+                <LineSeries
+                  data={this.state.history}
+                  color={"url(#myGradient)"}
+                  style={{
+                    strokeWidth: "7px",
+                    opacity: "0.8"
+                  }}
+                />
+              </FlexibleXYPlot>
+            )}
+          </div>
+        </Fragment>
+      );
     } else if (prevPrice.x > newPrice.x) {
-      return <Fall>{formatPrice(newPrice.x)}</Fall>;
+      return (
+        <Fragment>
+          <Fall>{formatPrice(newPrice.x)}</Fall>
+          <div>
+            {this.state.history && (
+              <FlexibleXYPlot height={200}>
+                {gradient}
+                <LineSeries data={this.state.history} color={"powderblue"} />
+              </FlexibleXYPlot>
+            )}
+          </div>
+        </Fragment>
+      );
     } else if (prevPrice.x < newPrice.x) {
-      return <Rise>{formatPrice(newPrice.x)}</Rise>;
+      return (
+        <Fragment>
+          <Rise>{formatPrice(newPrice.x)}</Rise>
+          <div>
+            {this.state.history && (
+              <FlexibleXYPlot height={200}>
+                {gradient}
+                <LineSeries data={this.state.history} color={"powderblue"} />
+              </FlexibleXYPlot>
+            )}
+          </div>
+        </Fragment>
+      );
     }
   }
 }
